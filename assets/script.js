be662c69286a53cdc94069deca32c921009fc786
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-10-13 22:46:19 +0100

    elif statement finished

--- assets/script.js
+++ assets/script.js
@@ -62,5 +62,16 @@ const game = () => {
                 playerScoreBoard.textContent = playerScore;
             }
         }
+        else if (player == 'paper') {
+            if (computer == 'scissors') {
+                result.textContent = 'Computer Won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            } else {
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }
+        }
     }
 }
