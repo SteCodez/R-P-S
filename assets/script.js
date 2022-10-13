ae49ad3ebec6e816e0896335baa374e3647e505a
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-10-13 23:37:21 +0100

    Adding defeat conditions and defeat message

--- assets/script.js
+++ assets/script.js
@@ -93,5 +93,9 @@ const game = () => {
             result.innerText = 'You Won The Game'
             result.style.color = '#308D46';
         }
+        else if (playerScore < computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Lost The Game';
+            result.style.color = 'red';
+        }
     }
-}
